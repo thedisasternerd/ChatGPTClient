aa46d15a2863ce3ac3f592438f18e5a1ad8d33bb
mathadoor <matharooh2@gmail.com>
2023-03-11 16:50:55 -0500

    MVP Completed

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-## Project Planning
-Objective: Build an application similar to ChatGPT. 
+## ChatGPT
+Objective: The plan is to build a single chat client application that mimics the similar behavior as ChatGPT.  Inteface will consists of an input bar at the bottom of the page. The user can type out their message and press enter/click send to receive a response. The responses are rendered via ReactMarkdown to render the code blocks and tables appropriately. Back End Interface: the back-end essentially sends requests to gpt-3.5-turbo.
 
-Milestones:
-  - Front End Interface
-  - Back End Interface
+## Tasks
+- [x] Minimum Viable Product:
+  - [x] Create the React App
+  - [x] Create the Chat Window
+  - [x] Create the input bar
+  - [x] Add new containers upon pressing enter and pressing the send button
 
-Front End Interface
-What does the Front End Interface consists of?
-Sidebar: to manage the chat history. For now the historical chats will be saved in a list ordered by the recency of the usage. 
+- [ ] Enhance the MVP
+  - [ ] Add the option to regenerate response
+  - [ ] Freeze the send button by replacing it with a spinning wheel. 
+  - [ ] Stream and animate the text
+  - [ ] Highlight Sendbutton when hovering over
+  - [ ] Route the request to ChatGPT API
+  - [ ] Collapsible Sidebar to set the other parameters such as temperature, top_p, etc.
 
-Backend:
-    - API Calls to the server
-      - Can already do that
-    - Storing Chat history in a database
-    - Respond to the requests from the Front-End
+Here is a screenshot of the MVP:
+<!-- Add Image from assets/ -->
+![ChatGPT](assets/demo_mvp.png)
--- src/activechat.js
+++ src/activechat.js
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import {BsSend} from 'react-icons/bs';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import './App.css';
+import './chatresponse.css';
 
 
 function FormContainer() {
   // State to store the list of containers
   const [containers, setContainers] = useState([]);
   const [value, setValue] = useState('');
+  const [response, setResponse] = useState('');
 
   useEffect(() => {
+
     const textarea = document.querySelector('.input_bar');
     const chatinput = document.querySelector('.chatinput');
     const chathistorymargin = document.querySelector('.chathistory');
+    const submitbutton = document.querySelector('.submit');
     
     const adjustHeight = () => {
       textarea.style.height = "auto";
       textarea.style.height = `${textarea.scrollHeight}px`;
-      // chatinput.style.height = 'auto';
       chatinput.style.height = `${textarea.scrollHeight}px`;
       // set the margin of the chat history to the height of the chatinput
       chathistorymargin.style.marginBottom = `${textarea.clientHeight + 10}px`;
-      
+    };
+
+    const resetHeight = () => {
+      textarea.style.height = '20px';
+      chatinput.style.height = textarea.style.height;
+      // set the margin of the chat history to the height of the chatinput
+      chathistorymargin.style.marginBottom = `${48}px`;
     };
 
     textarea.addEventListener('input', adjustHeight);
     textarea.addEventListener('keydown', adjustHeight);
+    submitbutton.addEventListener('click', resetHeight);
 
     return () => {
       textarea.removeEventListener('input', adjustHeight);
       textarea.removeEventListener('keydown', adjustHeight);
+      submitbutton.removeEventListener('click', adjustHeight);
     };
   }, []);
 
@@ -41,29 +54,70 @@ function FormContainer() {
     setValue(event.target.value);
   }
 
+  function handleEnter(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleAddContainer(event);
+    }
+  }
+  
+  useEffect(() => {
+    const chathistory = document.querySelector('.chathistory');
+    chathistory.scrollTop = chathistory.scrollHeight;
+  }, [response]);
+
+
+  // Define the handleSubmit function outside handleAddContainer  
+  
+  
   // Event handler for adding a new container
-  function handleAddContainer() {
-    const newContainer = (<div className="chatblock">
-                              <div className="usercontainer">
-                                <div className="useridentifier">
-                                  <text >🧔</text>
-                                </div>
-                                <div className="usermessage">
-                                  {value}
-                                </div>
-                              </div>
-                              <div className="agentcontainer">
-                                <div className="agentidentifier">
-                                  <text>🤖</text>
-                                </div>
-                                <div className="agentmessage">
-                                  <text className="message">{value}</text>
-                                </div>
-                              </div>
-                          </div>
-                          );
+  async function handleAddContainer(event) {
+    // Call handleSubmit with the event object
+    try{
+
+      const res = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer " + "API_KEY"
+        },
+        body: JSON.stringify({
+          "model": "gpt-3.5-turbo",
+          "messages": [{"role": "user", "content": value}]
+        })
+      });
+      const data = await res.json();
+      setResponse(data.choices[0].message.content);
+      console.log(data);
+      console.log(response);
+    const newContainer = (
+      <div className="chatblock">
+        <div className="usercontainer">
+          <div className="useridentifier">
+            <text >🧔</text>
+          </div>
+          <div className="usermessage">
+            {value}
+          </div>
+        </div>
+        <div className="agentcontainer">
+          <div className="agentidentifier">
+            <text>🤖</text>
+          </div>
+          <div className="agentmessage">
+            <ReactMarkdown className="markdown-content" remarkPlugins={[remarkGfm]} children={data.choices[0].message.content} />
+            {/* <text>{data.choices[0].message.content}</text> */}
+          </div>
+        </div>
+      </div>
+    );
+   
     setValue("");
     setContainers([...containers, newContainer]);
+    }
+    catch(err){
+      console.log(err);
+    }
   }
 
   // Event handler for submitting the bottom form
@@ -84,7 +138,7 @@ function FormContainer() {
         <form onSubmit={handleBottomFormSubmit}>
           {/* Code to render bottom form */}
           <div className="chatinput">
-              <textarea className="input_bar" value={value} onChange={handleChange}/>
+              <textarea className="input_bar" value={value} onChange={handleChange} onKeyDown={handleEnter}/>
               <button className="submit" onClick={handleAddContainer} ><BsSend className="icon"/></button>
           </div>
           
